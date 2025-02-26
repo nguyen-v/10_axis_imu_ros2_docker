3606bdfbe8f4645804ae1dd1640fca9c9b78f390
Vincent Nguyen <vincen7.nguyen@gmail.com>
2025-02-26 18:16:24 +0100

    mount device directly

--- docker-run.sh
+++ docker-run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Resolve the actual device that /dev/imu points to
 docker run -t --rm \
-  --privileged \
-  -v=/dev/sensors:/dev/sensors \
+  --device=/dev/sensors/imu \
   imu_ros2:latest \
   ros2 run imu imu_node --ros-args --param port_name:=/dev/sensors/imu
