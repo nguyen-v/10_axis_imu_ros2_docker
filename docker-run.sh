bd70b9329934b0b6225bf4f9b478d51ab4f0dd9f
Vincent Nguyen <vincen7.nguyen@gmail.com>
2025-02-26 18:00:41 +0100

    Add udev rule in subfolder

--- docker-run.sh
+++ docker-run.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 # Resolve the actual device that /dev/imu points to
-DEVICE=$(readlink -f /dev/imu)
-
-if [ -z "$DEVICE" ]; then
-  echo "Error: /dev/imu not found. Please ensure your udev rule is working."
-  exit 1
-fi
-
-echo "10-axis ROS IMU detected device: $DEVICE"
-
-docker run -it --rm \
+docker run -t --rm \
   --privileged \
-  --device=$DEVICE \
+  -v=/dev/sensors:/dev/sensors \
   imu_ros2:latest \
-  ros2 run imu imu_node --ros-args --param port_name:=$DEVICE
+  ros2 run imu imu_node --ros-args --param port_name:=/dev/sensors/imu
